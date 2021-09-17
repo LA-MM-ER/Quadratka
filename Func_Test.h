6269192babdde5771bc7553e989d2055f6d46208
LA-MM-ER <89412923+LA-MM-ER@users.noreply.github.com>
2021-09-17 15:22:42 +0300

    Add files via upload

---
+++ Func_Test.h
@@ -0,0 +1,86 @@
+#include "Functions.cpp"
+#include "Tests.cpp"
+
+//! @brief This function compares <double> variables with zero
+//! @param [in] eps - the precision with which we measure a variable
+//! @return 1 or 0 depending on the result of comparison with <eps>
+
+bool is_zero(double number);
+
+
+
+
+//! @brief This function solves the square equation
+//! @return number of roots
+//! @param [in] a, b, c - coefficients of equation
+//! @param [out] x1, x2 - roots of equation
+
+int solve_quadratic (double a, double b, double c, double *x1, double *x2);
+
+
+
+
+//! @brief This function compares two <double> numbers
+//! @return 1 if they are equal and 0 if not
+//! @param [in] a, b - <double> numbers
+
+int is_equal(double a, double b);
+
+
+
+
+//! @brief This test checks functionality of the function in standard situation
+//! @param [in] a,b,c - coefficients of equation
+//! @param [in] x1_true, x2_true - roots that we know
+//! @param [in] x1_end, x2_end - variables for roots
+//! @return success status of test (1 if test is success and 0 if not)
+
+int test_classic();
+
+
+
+
+//! @brief This test checks functionality of the function when all coefficient variables equals zero
+//! @param [in] a,b,c - coefficients of equation
+//! @param [in] x1_end, x2_end - variables for roots
+//! @return success status of test (1 if test is success and 0 if not)
+
+int test_infinite();
+
+
+
+
+//! @brief This test checks functionality of the function when a equals 0 and b,c is not
+//! @param [in] a,b,c - coefficients of equation
+//! @param [in] x1_end, x2_end - variables for roots
+//! @param [in] x1_true - root that we know
+//! @return success status of test (1 if test is success and 0 if not)
+
+int test_a_zero_only();
+
+
+
+
+//! @brief This test checks functionality of the function when a, b equals 0 and c is not
+//! @param [in] a,b,c - coefficients of equation
+//! @param [in] x1, x2 - variables for roots
+//! @return success status of test (1 if test is success and 0 if not)
+
+int test_ab_zero_cnot();
+
+
+
+
+//! @brief This test checks functionality of the function with negative discriminant
+//! @param [in] a,b,c - coefficients of equation
+//! @param [in] x1, x2 - variables for roots
+//! @return success status of test (1 if test is success and 0 if not)
+
+int test_negative_discr();
+
+
+
+
+//! @brief This function run all tests at once
+
+int UnitTest();
---
+++ Functions.cpp
@@ -0,0 +1,66 @@
+extern const int INFINITE_ROOTS = 3;
+
+bool is_zero(double number) {
+
+    double eps = 0.00001;
+
+    return (fabs(number) < eps);
+}
+
+int solve_quadratic (double a, double b, double c, double *x1, double *x2) {
+
+    assert(x1 != NULL);
+
+    if (is_zero(a)) {
+
+        if (is_zero(b)) {
+
+            if (is_zero(c)) {
+
+                return INFINITE_ROOTS;
+
+            } else {
+
+                return 0;
+            }
+
+        } else {
+
+            *x1 = (-1 * c / b);
+
+            return 1;
+        }
+
+    } else {
+
+        double discr = b * b - 4 * a * c;
+        double sqrt_discr = sqrt(discr);
+
+        if(discr >= 0) {
+
+            *x1 = ( -1 * b + sqrt_discr ) / (2 * a);
+            *x2 = ( -1 * b - sqrt_discr ) / (2 * a);
+
+            return 2;
+
+        } else {
+
+            return 0;
+
+        }
+    }
+
+
+}
+
+int is_equal(double a, double b) {
+    if (fabs(a - b) < 0.00001) {
+
+        return 1;
+    }
+
+    else {
+
+        return 0;
+    }
+}
